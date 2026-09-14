4825dd1883a68975e6b1524e4fbdcb8f3a142c56
Marco Minerva <marco.minerva@gmail.com>
2026-09-14 09:44:43 +0200

    Refactor AddRetryExecutor to block-bodied method
    Changed AddRetryExecutor from an expression-bodied to a block-bodied method, enclosing the existing logic in curly braces without altering its behavior.

--- src/SimpleRetry/ServiceCollectionExtensions.cs
+++ src/SimpleRetry/ServiceCollectionExtensions.cs
@@ -113,10 +113,12 @@ public static class ServiceCollectionExtensions
     }
 
     private static void AddRetryExecutor(IServiceCollection services)
-        => services.TryAddKeyedSingleton<IRetryExecutor>(KeyedService.AnyKey, (services, key) =>
+    {
+        services.TryAddKeyedSingleton<IRetryExecutor>(KeyedService.AnyKey, (services, key) =>
         {
             var options = services.GetKeyedService<RetryPolicyOptions>(key) ?? new RetryPolicyOptions();
             var loggerFactory = services.GetService<ILoggerFactory>() ?? NullLoggerFactory.Instance;
             return new DefaultRetryExecutor(options, services, loggerFactory);
         });
+    }
 }
